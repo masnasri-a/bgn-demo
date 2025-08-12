9dc536fe26586b5219351d9f7d33d7af72fe998a
aliendev <nasriblog12@gmail.com>
2025-08-12 20:35:08 +0700

    feat: implement data fetching for total and monthly report statistics in CardReport component

--- src/app/(user)/(others-pages)/report/cardReport.tsx
+++ src/app/(user)/(others-pages)/report/cardReport.tsx
@@ -1,8 +1,24 @@
+"use client"
 
 import { MessageSquare, DollarSign } from "lucide-react"
+import { useEffect, useState } from "react"
 import { HiDocument, HiDocumentReport } from "react-icons/hi"
 
 export default function CardReport() {
+	const [total, setTotal] = useState(0)
+	const [monthTotal, setMonthTotal] = useState(0)
+
+	useEffect(() => {
+		// Fetch data or perform side effects here
+		fetch(process.env.NEXT_PUBLIC_BASE_API + "/report/statistics")
+			.then(res => res.json())
+			.then(data => {
+				setTotal(data.total)
+				setMonthTotal(data.month_total)
+			})
+			.catch(err => console.error(err))
+	}, [])
+
 	return (
 		<div className="grid grid-cols-1 sm:grid-cols-3 gap-4 my-4">
 			{/* Card 1 */}
@@ -24,22 +40,22 @@ export default function CardReport() {
 
 			{/* Card 2 */}
 			<div className="bg-white dark:bg-gray-900 rounded-xl p-6 shadow-sm flex flex-col gap-2 border border-gray-100 dark:border-gray-800">
-				<div className="flex items-center mb-2">
+				<div className="flex items-center mb-4">
                     <div className="bg-blue-100 text-blue-600 rounded-lg p-2 mr-2">
                         <HiDocument size={24} />
                     </div>
 					<span className="font-semibold text-gray-800 dark:text-white text-base">Total Report</span>
 				</div>
-				<div className="text-2xl font-bold text-gray-800 dark:text-white mb-2">144 Report</div>
+				<div className="text-2xl font-bold text-gray-800 dark:text-white mb-2">{total} Report</div>
 			</div>
             <div className="bg-white dark:bg-gray-900 rounded-xl p-6 shadow-sm flex flex-col gap-2 border border-gray-100 dark:border-gray-800">
-				<div className="flex items-center mb-2">
+				<div className="flex items-center mb-4">
                     <div className="bg-blue-100 text-blue-600 rounded-lg p-2 mr-2">
                         <HiDocumentReport size={24} />
                     </div>
 					<span className="font-semibold text-gray-800 dark:text-white text-base">Total Report Bulan Ini</span>
 				</div>
-				<div className="text-2xl font-bold text-gray-800 dark:text-white mb-2">15 Report</div>
+				<div className="text-2xl font-bold text-gray-800 dark:text-white mb-2">{monthTotal} Report</div>
 			</div>
 		</div>
 	)
